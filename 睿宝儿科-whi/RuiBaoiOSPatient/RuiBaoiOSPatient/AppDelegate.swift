753e189b66ff29a79ca7dab1d528947ce515b070
whj <1032780361@qq.com>
2016-11-12 21:44:55 +0800

    添加了一些注释

--- 睿宝儿科-whi/RuiBaoiOSPatient/RuiBaoiOSPatient/AppDelegate.swift
+++ 睿宝儿科-whi/RuiBaoiOSPatient/RuiBaoiOSPatient/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         
+//        let vv = uivi
+//        casdcsdac
+//        dfas
+        
         // 确定中英文环境
         ChangeLanguage.shareChangeLanguage().toChangeLanguage()
 
